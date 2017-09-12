0e77945fb30b03f2d43af7d41e80915415914468
Tobias Bodor <tobias.bodor@gmail.com>
2017-09-12 08:32:04 +0200

    move project files to root

--- accounting-app/app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from app.models import Account, AccountingRecord, AccountingRecordBase
-
-admin.site.register(Account)
-admin.site.register(AccountingRecord)
-admin.site.register(AccountingRecordBase)
