ba1e9d57a3ea1f8e28d01a1d576b2e1c4cf0af36
Tobias Bodor <tobias.bodor@gmail.com>
2017-09-18 22:46:37 +0200

    add hash to bundle

--- accounting-app/webpack.config.production.js
+++ accounting-app/webpack.config.production.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/index.js',
 
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash].js',
         path: path.resolve(__dirname, 'static', 'app'),
         publicPath: '/static/app/'
     },
